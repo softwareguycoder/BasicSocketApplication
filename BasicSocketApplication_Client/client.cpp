07f45bfc6b434eb5b2334769a3b00dfd7e2a2b46
Brian Hart <brian.hart@xylogix.net>
2018-09-10 11:28:15 -0400

    10 Sep 2018 Connecting to a Socket

--- BasicSocketApplication_Client/client.cpp
+++ BasicSocketApplication_Client/client.cpp
@@ -55,6 +55,26 @@ int _cdecl main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	// Connect to server.
+	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+	}
+
+	// Should really try the next address returned by getaddrinfo
+	// if the connect call failed
+	// But for this simple example we just free the resources
+	// returned by getaddrinfo and print an error message
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		return 1;
+	}
+
 
 
 	return iResult;
