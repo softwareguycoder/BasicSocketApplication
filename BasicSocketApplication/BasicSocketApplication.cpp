8b4953a1682fd442f48ebee9d91c2feb2b560d96
Brian Hart <brian.hart@xylogix.net>
2018-09-10 10:50:33 -0400

    10 Sep 2018 Creating a Socket for the Server

--- BasicSocketApplication/BasicSocketApplication.cpp
+++ BasicSocketApplication/BasicSocketApplication.cpp
@@ -10,8 +10,11 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-int main() {
+#define DEFAULT_PORT "27015"
+
+int _cdecl main() {
 	WSADATA wsaData;
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
 
 	int iResult = 0;
 
@@ -19,6 +22,33 @@ int main() {
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
 		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+
+	// Create a SOCKET for the server to listen for client connections
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
 		return 1;
 	}
 
