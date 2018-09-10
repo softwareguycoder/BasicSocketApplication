534aaa062dee25a432bdcf756c88c1737eeb7e9f
Brian Hart <brian.hart@xylogix.net>
2018-09-10 11:27:12 -0400

    10 Sep 2018 Creating a Socket for the Client

---
+++ BasicSocketApplication_Client/client.cpp
@@ -0,0 +1,61 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_PORT "27015"
+
+int _cdecl main(int argc, char *argv[]) {
+	WSADATA wsaData;
+	int iResult;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	// Attempt to connect to the first address returned by
+	// the call to getaddrinfo
+	ptr = result;
+
+	// Create a SOCKET for connecting to server
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+		ptr->ai_protocol);
+
+	// Check for errors to ensure that the ConnectSocket is a 
+	// valid socket.
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+
+
+	return iResult;
+}
